d8b5b6ecfd7d5e95a5784af292f4a5048d6f6eca
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2025-02-24 15:31:03 +0100

    Ajout des fichiers nécessaires pour le projet VPub

---
+++ VPub_t/test/java/pub/Bar_test.java
@@ -0,0 +1,28 @@
+package pub;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Bar_test {
+
+    Bar monBar = new Bar();
+    Boisson cafe=new Boisson("cafe");
+    Cocktail mojito = new Cocktail ("mojito");
+    @Test
+    public void testAjoutBoisson() {
+        monBar.add(cafe);
+        assertTrue(monBar.boissonFroide.contains(cafe));
+    }
+    @Test
+    public void testAjoutcocktail() {
+        monBar.add(mojito);
+        assertTrue(monBar.cocktailSansAlcoole.contains(mojito));
+    }
+    @Test
+    public void testtoString() {
+        monBar.add(mojito);
+        monBar.add(cafe);
+        System.out.println(monBar.toString());
+    }
+}
